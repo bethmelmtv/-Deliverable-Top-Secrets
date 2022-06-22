a96e90440d8438e136e876d518cfb5cadfc6213a
Beth <bethmelmtv@gmail.com>
2022-06-22 16:36:45 -0700

    wrote a test, controller, model for creating a new user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,14 +3,30 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const aUser = {
+  firstName: 'Beth',
+  lastName: 'Mel',
+  email: 'beth@beth.com',
+  password: '1234567',
+};
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
   afterAll(() => {
     pool.end();
+  });
+
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(aUser);
+    const { firstName, lastName, email } = aUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
 });
