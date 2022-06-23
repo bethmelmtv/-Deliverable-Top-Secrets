883d8f8b6b9d88cafa1cd9f427a4738044803f4e
Beth <bethmelmtv@gmail.com>
2022-06-22 17:49:02 -0700

    added test and userservice file for signing in

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const aUser = {
   firstName: 'Beth',
@@ -27,6 +28,27 @@ describe('backend-express-template routes', () => {
       firstName,
       lastName,
       email,
+    });
+  });
+
+  const registerAndLogin = async (userProps = {}) => {
+    const password = userProps.password ?? aUser.password;
+    const agent = request.agent(app);
+    const user = await UserService.create({ ...aUser, ...userProps });
+
+    const { email } = user;
+    await agent.post('/api/v1/users/sessions').send({ email, password });
+    return [agent, user];
+  };
+
+  it('returns the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
     });
   });
 });
